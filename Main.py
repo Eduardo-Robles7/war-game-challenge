5f604b24077e5928c251c7eb4db68e4008f9c766
Eduardo Robles <eduardorobles575@gmail.com>
2020-01-25 16:44:27 -0800

    Implemented basic case for multiple wars

--- Main.py
+++ Main.py
@@ -21,22 +21,42 @@ def main():
         cpu_card = cpu.pop_card()
         
         #display cards
-        print('\n{}: {}'.format(player.name, player_card.show()))
-        print('{}: {}'.format(cpu.name, cpu_card.show()))
+        print('{}: {}\n'.format(player.name, player_card.show()))
+        print('{}: {}\n'.format(cpu.name, cpu_card.show()))
 
         if(player_card == cpu_card):
-            print('\n*********************************************************WAR')
-            #player.add_card(player_card)
-            #cpu.add_card(cpu_card)
-        elif(player_card > cpu_card):
-            print('\nPlayer Wins Round')
-            player_stash.extend((player_card, cpu_card))
-        else:
-            print('\nCPU Wins Round')
+            print('\nWAR\n')
+            tied = True
+            war_cards = []
+            while(tied):
+                if(not player.has_cards() or not cpu.has_cards()):
+                    tied == False
+                    break
 
-        rounds = rounds + 1
-     
-        print('---------------------')
+                player_card1 = player.pop_card()
+                player_card2 = player.pop_card()
+                cpu_card1 = cpu.pop_card()
+                cpu_card2 = cpu.pop_card()
+
+                war_cards.extend([player_card1, player_card2, cpu_card1, cpu_card2])
+
+                if(player_card2 > cpu_card2):
+                    player_stash.extend(war_cards)
+                    print('\n{} Wins War'.format(player.name))
+                    tied = False
+                elif(player_card2 < cpu_card2):
+                    cpu_stash.extend([war_cards])
+                    print('\n{} Wins Round'.format(cpu.name))
+                    tied = False
+
+        elif(player_card > cpu_card):
+            player_stash.extend((player_card, cpu_card))
+            print('\n{} Wins Round'.format(player.name))
+        else:
+            cpu_stash.extend((player_card, cpu_card))
+            print('\n{} Wins Round'.format(cpu.name))
+
+        print('---------------------------------')
 
     
 
